5b08e103413ade2c57af5b2f1c51c26332ec691a
Xavier Cambar <xcambar@gmail.com>
2015-09-08 23:57:41 +0200

    fixes root path

--- tests/runner.js
+++ tests/runner.js
@@ -13,7 +13,7 @@ if (process.env.EOLNEWLINE) {
 rimraf.sync('.node_modules-tmp');
 rimraf.sync('.bower_components-tmp');
 
-var root = 'tests/{unit,acceptance}';
+var root = 'tests';
 var _checkOnlyInTests = RSVP.denodeify(mochaOnlyDetector.checkFolder.bind(null, root + '/**/*{-test,-slow}.js'));
 var optionOrFile = process.argv[2];
 var mocha = new Mocha({
